4f9fcc9149fb3cd751e34a8da521a354d5e31bdb
Jesse Shaw <JesseShawCodes@Gmail.com>
2024-06-15 21:51:34 -0400

    Removed defaults stylings, added Canvas to index file

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <main>
+    <canvas id="defaultCanvas0" className="p5Canvas" width="3072" height="694" style={{"width": '1536px', 'height': '347px'}}></canvas>
+  </main>
 );
 
 // If you want to start measuring performance in your app, pass a function
